d0707f03c88a90934180e80e11956d5c576debba
Anton Kraev <antonkraeww@gmail.com>
2022-12-15 09:32:20 +0300

    added samples and separation of states between tasks

--- model.py
+++ model.py
@@ -6,9 +6,33 @@ from PIL import Image
 
 from diffusers import StableDiffusionPipeline
 
-pipe = StableDiffusionPipeline.from_pretrained(
-    "C:\\Users\\anton\\stable-diffusion-v1-4",
-    custom_pipeline="stable_diffusion_mega",
-)
-pipe.to("cuda" if torch.cuda.is_available() else "cpu")
-pipe.enable_attention_slicing()
+
+class Model:
+    def __init__(self):
+        self.pipe = StableDiffusionPipeline.from_pretrained(
+            "C:\\Users\\anton\\stable-diffusion-v1-4",
+            custom_pipeline="stable_diffusion_mega",
+        )
+        self.pipe.to("cuda" if torch.cuda.is_available() else "cpu")
+        self.pipe.enable_attention_slicing()
+
+    def generate(self, pipeline, params):
+        prompt = params['prompt']
+        image = params['init_img']
+        mask_image = params['mask_img']
+        negative_prompt = params['negative_prompt']
+        guidance_scale = params['guidance_scale']
+        num_inference_steps = params['num_inference_steps']
+        num_images_per_prompt = params['num_images_per_prompt']
+        if pipeline == 'text2img':
+            return self.pipe.text2img(prompt=prompt, negative_prompt=negative_prompt, guidance_scale=guidance_scale,
+                                      num_inference_steps=num_inference_steps,
+                                      num_images_per_prompt=num_images_per_prompt).images
+        if pipeline == 'img2img':
+            return self.pipe.img2img(prompt=prompt, image=image, negative_prompt=negative_prompt,
+                                     guidance_scale=guidance_scale, num_inference_steps=num_inference_steps,
+                                     num_images_per_prompt=num_images_per_prompt).images
+        if pipeline == 'inpaint':
+            return self.pipe.inpaint(prompt=prompt, image=image, mask_image=mask_image, negative_prompt=negative_prompt,
+                                     guidance_scale=guidance_scale, num_inference_steps=num_inference_steps,
+                                     num_images_per_prompt=num_images_per_prompt).images
